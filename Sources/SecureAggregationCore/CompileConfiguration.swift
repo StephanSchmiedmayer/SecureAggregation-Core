f7985e53186ca97048c363301a56be281c12dcd0
Stephan Schmiedmayer <ge25bih@mytum.de>
2021-08-29 18:14:11 +0200

    Add typealias SAPubKeyCurve and UserID

---
+++ Sources/SecureAggregationCore/CompileConfiguration.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by stephan on 20.08.21.
+//
+
+import Foundation
+import CryptoKit
+
+public typealias SAPubKeyCurve = Curve25519
+public typealias UserID = Int
+//public typealias UserAuthentication = Int
