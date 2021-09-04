584eab1ea27196551ea201cae622bbaa693650a3
Stephan Schmiedmayer <ge25bih@mytum.de>
2021-09-05 01:20:30 +0200

    Change SAWrappedValue.mask back to only take seed & mod, instead add required instance method .cancelling(ownID:otherID:)

--- Sources/SecureAggregationCore/SAValue/SAWrappedValueProtocol.swift
+++ Sources/SecureAggregationCore/SAValue/SAWrappedValueProtocol.swift
@@ -19,24 +19,29 @@ public protocol SAWrappedValue: Content {
     init(_ value: Value)
         
     // MARK: - arithmetic operations
+    
     /// Modulo addition
     func add(_ rhs: Self, mod: Modulus) -> Self
     /// Modulo subtraction
     func sub(_ rhs: Self, mod: Modulus) -> Self
-
+    
+    
     // MARK: - cryptographic operations
-    /// Creates a mask from a seed
+    
+    /// Needs ot take into account ownID and otherID so that for any valid  `seed`, `mod`, `a`, `b`:
     ///
-    /// Needs ot take into account ownID and otherID so that for any valid `seed`, `mod`, `a`, `b`
-    ///
-    ///     let maskA = mask(seed, mod, a, b)
-    ///     let maskB = mask(seed, mod, b, a)
+    ///     let mask = mask(seed, mod)
+    ///     let maskA = mask.cancelling(ownID: a, otherID: b)
+    ///     let maskB = mask.cancelling(ownID: b, otherID: a)
     ///     let zero = maskA.add(maskB, mod)
     ///     value.add(zero, mod) == value
     ///
-    /// holds (meaning x is a representation of 0 / masks cancel each other out). This is curtial to SecureAggregation.
+    /// holds (meaning zero is a representation of 0 / masks cancel each other out). This is curtial to SecureAggregation.
+    ///
+    func cancelling(ownID: UserID, otherID: UserID) -> Self
+    
+    /// Creates a mask from a seed
     ///
     /// - Important: Ciritical for Security of the protocol
-    ///
-    static func mask(forSeed seed: SharedSecret, mod: Modulus, ownID: UserID, otherID: UserID) -> Self
+    static func mask(forSeed seed: SharedSecret, mod: Modulus) -> Self
 }
